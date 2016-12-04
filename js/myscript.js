5a637ac06c778d347bbf550d2f1f43898a833302
vida22 <Vida@Vidas-MacBook-Pro.local>
2016-12-04 03:19:18 -0500

    previous, next, back home

--- js/myscript.js
+++ js/myscript.js
@@ -17,50 +17,61 @@ $(document).on('ready', function() {
   });
 
 //Hide and show nav when scroll
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('nav').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').removeClass('nav-down').addClass('nav-up border');
-        $('.project-sum').addClass('hidden');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('nav-up').addClass('nav-down');
-            $('.project-sum').removeClass('hidden');
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
         }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').removeClass('nav-down').addClass('nav-up border');
+            $('.project-sum').addClass('hidden');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up').addClass('nav-down');
+                $('.project-sum').removeClass('hidden');
+            }
+        }
+
+        //remove nav border when at the top of the page 
+        if (st < 60){
+            $('nav').removeClass('border');
+        }
+        
+        //show nav if at page bottom
+        if (st > max-200){
+            $('nav').removeClass('nav-up').addClass('border');
+        }
+        
+        if (st > max*.70){
+            $('.back-home').removeClass('hidden');
+        } else {
+            $('.back-home').addClass('hidden')
+        }
+
+        lastScrollTop = st;
     }
-    
-    //remove nav border when at the top of the page 
-    if (st < 60){
-        $('nav').removeClass('border');
-    }
-    
-    lastScrollTop = st;
-}
-    
+
     
 });
