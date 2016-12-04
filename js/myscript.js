b6795cf050913a712934de9b684fb7fe0c293af0
vida22 <Vida@Vidas-MacBook-Pro.local>
2016-12-03 21:26:24 -0500

    progress bar, moving nav

---
+++ js/myscript.js
@@ -0,0 +1,65 @@
+$(document).on('ready', function() {
+    
+//Progress Bar    
+  var winHeight = $(window).height(), 
+      docHeight = $(document).height(),
+      progressBar = $('progress'),
+      max, value;
+
+  /* Set the max scrollable area */
+  max = docHeight - winHeight;
+  progressBar.attr('max', max);
+    
+
+  $(document).on('scroll', function(){
+     value = $(window).scrollTop();
+     progressBar.attr('value', value);
+  });
+
+//Hide and show nav when scroll
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    console.log("ran...");
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up border');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    //remove nav border when at the top of the page 
+    if (st < 60){
+        $('nav').removeClass('border');
+    }
+    
+    lastScrollTop = st;
+}
+    
+    
+});
